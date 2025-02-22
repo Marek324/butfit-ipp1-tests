7329b1e1eca3e6b71fc26039a4e7c6597053a13c
Marek324 <hrmajo1@gmail.com>
2025-02-22 15:49:53 +0100

    Fix XML comparison

--- utils_tests.py
+++ utils_tests.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import subprocess
+import re
 
 def run_arg_test(args, expected_code):
     args = ["python3.11", "parse.py"] + args
@@ -30,6 +31,19 @@ def run_test(input, expected_code):
     
     assert len(process.stderr) != 0
 
+
+def str_to_xml_s(x):
+    return  ET.tostring(
+            ET.fromstring(
+            re.sub(r'&nbsp;', '\\\\n',
+            re.sub(r'\s+=\s+', '=',
+            re.sub(r'<\s+', '<',
+            re.sub(r'\s+>', '>',
+            re.sub(r'</\s+', '</',
+            re.sub(r'\s+\/>', '/>',
+            "".join(re.findall(r'<[^<>]*>', x.strip()))
+            )))))))).decode('utf-8')
+
 def compare_xml_elements(elem1, elem2):
     if elem1.tag != elem2.tag:
         return False
@@ -44,8 +58,8 @@ def compare_xml_elements(elem1, elem2):
 
 def compare_xml_strings(xml_string1, xml_string2):
     try:
-        elem1 = ET.fromstring(xml_string1.strip())
-        elem2 = ET.fromstring(xml_string2.strip())
+        elem1 = ET.fromstring(str_to_xml_s(xml_string1))
+        elem2 = ET.fromstring(str_to_xml_s(xml_string2))
         return compare_xml_elements(elem1, elem2)
     except ET.ParseError:
         return False
